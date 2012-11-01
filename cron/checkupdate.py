1ca4e9816e21c38fb0a87a48c1a7975007b38885
livelazily <livelazily@gmail.com>
2012-11-01 10:05:53 +0800

    logging exception

--- cron/checkupdate.py
+++ cron/checkupdate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'livelazily'
 
+import logging
 from datetime import datetime
 from urlparse import urljoin
 
@@ -8,52 +9,47 @@ from lxml import etree
 from models import GAEProxy
 from google.appengine.ext import db
 from google.appengine.api import mail
+from google.appengine.api.urlfetch import fetch
 
 def getLastFile():
-    url = "http://code.google.com/p/gaeproxy/downloads/list?can=2&q=&colspec=Filename"
+    url = "https://code.google.com/p/gaeproxy/downloads/list?can=2&q=&colspec=Filename"
     try:
-        root = etree.parse(url, parser=etree.HTMLParser())
+        root = etree.fromstring(fetch(url=url).content, parser=etree.HTMLParser())
         cols = root.xpath('//*[@id="resultstable"]/tr[1]/td')
         file_link = cols[1].find('a')
         file_name = file_link.text.strip()
         detial_url = urljoin(url, file_link.get('href'))
         return file_name, detial_url
     except Exception as e:
-        print(e)
-        return '', ''
+        logging.exception(e)
+        raise
 
 
 def getDetialData(url):
-    """"""
     try:
-        root = etree.parse(url, parser=etree.HTMLParser())
+        root = etree.fromstring(fetch(url=url).content, parser=etree.HTMLParser())
         rows = root.xpath('//*[@id="color_control"]/table/tbody/tr/td[2]/table/tr')
 
         file_link = rows[0].find('td/div/div[4]/a[1]').get('href')
-        file_link = db.Link(urljoin(url,file_link))
+        file_link = db.Link(urljoin(url, file_link))
         sha1 = rows[2].find('td').text
         desc = rows[1].find('td/pre').text
         return {'url': file_link, 'sha1': sha1, 'desc': desc, 'date': datetime.now()}
     except Exception as e:
-        print(e)
-        return {}
+        logging.exception(e)
+        raise
 
 
-def sendEmail(name,link):
+def sendEmail(name, link):
     message = mail.EmailMessage()
-
-    message.sender = 'livelazily@gmail.com'
+    message.sender = 'livelazily <livelazily@gmail.com>'
     message.to = 'livelazily@gmail.com'
     message.subject = u'GAEProxy 有新的版本 %s' % name
-    message.body = u'''GAEProxy 有新的版本 %s 可以下载了
-    下载地址为 %s
-    ''' % (name,link)
-
+    message.body = u'GAEProxy 有新的版本 %s 可以下载了\n下载地址为 %s' % (name, link)
     message.send()
 
 
-
-if __name__ == "__main__":
+def checkUpdate():
     file_name, detial_url = getLastFile()
     if file_name:
         query = db.GqlQuery('SELECT * FROM GAEProxy WHERE name = :1', file_name)
@@ -62,9 +58,24 @@ if __name__ == "__main__":
             data = getDetialData(detial_url)
             new_gae = GAEProxy(name=file_name, **data)
             key = new_gae.put()
-            sendEmail(file_name,data['url'])
+            sendEmail(file_name, data['url'])
             print('new gae file key is %s' % str(key.id_or_name()))
         else:
             key = old_gae.key()
             print('old gae file key is %s' % str(key.id_or_name()))
     print('<br> %s' % file_name)
+
+
+def main():
+#    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(asctime)s %(message)s',
+#                        datefmt='[%b %d %H:%M:%S]')
+    logging.getLogger().setLevel(logging.DEBUG)
+    try:
+        checkUpdate()
+    except Exception,e:
+        logging.exception(e)
+        print("check new version of GAEProxy faild!")
+
+
+if __name__ == "__main__":
+    main()
